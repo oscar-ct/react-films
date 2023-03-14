148646ac07fc729d3d9172651cf480c493ffe03e
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-14 00:04:39 -0500

    feat: fixed daisy ui issue, setup search component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {OMDbProvider} from "./components/context/OMDbContext";
 import Home from "./components/pages/Home";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
     return (
         <>
             <OMDbProvider>
                 <Router>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                    </Routes>
+                    <Navbar/>
+                    <main className="container mx-auto px-3 pb-12">
+                        <div className="flex flex-col justify-between h-screen bg-accent-content/10">
+                            <Routes>
+                                <Route path="/" element={<Home/>}/>
+                            </Routes>
+                        </div>
+                    </main>
                 </Router>
             </OMDbProvider>
         </>
--- src/components/films/FilmSearch.jsx
+++ src/components/films/FilmSearch.jsx
@@ -1,14 +1,41 @@
 import { filmSearch } from "../context/OMDbActions";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 
 const FilmSearch = () => {
 
-
+    const {text, setText} = useState();
     const fetchFilms =  filmSearch("tron")
 
+    const handleSubmit = () => {
+
+    }
+
+    const handleChange = () => {
+
+    }
+
     return (
-        <div>
+        <div className="grid grid-cols-1 xl:grids-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-control">
+                        <div className="relative">
+                            <input
+                                className="w-full pr-40 bg-gray-500 input input-lg text-white"
+                                placeholder="Search"
+                                value={text}
+                                onChange={handleChange}
+                            />
+                            <button className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">
+                                Go
+                            </button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+            {/* CLEAR button */}
 
         </div>
     );
