31213332025280f7f7cffe0a7df76d4a6a2b2f16
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-14 02:52:54 -0500

    setup spinner gif

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -1,10 +1,16 @@
-
+import spinner from "./assets/spinner2.gif";
 
 const Spinner = () => {
     return (
-        <div>
-
+        <div className="w-100 mt-20">
+            <img
+                src={spinner}
+                alt="Loading..."
+                className="text-center mx-auto"
+                width={180}
+            />
         </div>
+
     );
 };
 
