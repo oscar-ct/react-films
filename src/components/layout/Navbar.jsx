d572384867b40c5bda0def85c86f266de75a8907
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-16 14:36:10 -0500

    feat: styled nav and footer

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -18,25 +18,37 @@ const Navbar = () => {
                                 <Link to="/"><FaHome/>Home</Link>
                             </li>
                             <li>
-                                <Link to="/"><FaAddressCard/>About</Link>
+                                <Link to="/"><FaAddressCard/>Contact</Link>
                             </li>
                         </ul>
                     </div>
                     <div className="hidden lg:flex">
                         {/*<ul className="menu menu-horizontal px-1">*/}
                         {/*    <li>*/}
-                                <Link to="/">
-                                    <button className="btn btn-ghost btn-circle ml-2">
-                                        <FaHome className="text-2xl"/>
-                                    </button>
-                                </Link>
-                            {/*</li>*/}
-                            {/*<li>*/}
-                                <Link to="/">
-                                    <button className="btn btn-ghost btn-circle ml-2">
+                            <Link to="/">
+                                <div className="flex justify-center items-center ">
+                                    <button className="btn btn-ghost btn-circle">
                                         <FaAddressCard className="text-2xl"/>
                                     </button>
-                                </Link>
+                                    <span className="text-md hover:font-bold">Contact</span>
+                                </div>
+                            </Link>
+                            <Link to="/">
+                                <div className="flex justify-center items-center ml-4">
+                                    <button className="btn btn-ghost btn-circle">
+                                        <FaHome className="text-2xl"/>
+                                    </button>
+                                    <span className="text-md hover:font-bold">Home</span>
+                                </div>
+                            </Link>
+
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    <Link to="/">*/}
+                            {/*        <button className="btn btn-ghost btn-circle ml-2">*/}
+                            {/*            <FaAddressCard className="text-2xl"/>*/}
+                            {/*        </button>*/}
+                            {/*    </Link>*/}
                             {/*</li>*/}
                         {/*</ul>*/}
                     </div>
@@ -59,7 +71,7 @@ const Navbar = () => {
                     <FilmSearch/>
                     <button className="btn btn-ghost btn-circle">
                         <div className="indicator">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
                             <span className="badge badge-xs badge-primary indicator-item"/>
                         </div>
                     </button>
--- src/components/omdb-films/FilmSearch.jsx
+++ src/components/omdb-films/FilmSearch.jsx
@@ -32,12 +32,12 @@ const FilmSearch = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit} className="flex">
+        <form onSubmit={handleSubmit} className="flex flex-row">
             <div>
                 <input
                     type="text"
                     placeholder="Search Titles"
-                    className="input input-bordered bg-black text-white"
+                    className="input input-bordered rounded border-0 w-75 text-white bg-gradient-to-r from-black to-neutral text-md"
                     value={text}
                     onChange={handleChange}
                 />
@@ -46,7 +46,7 @@ const FilmSearch = () => {
                 <button onClick={() => {
                     onClick();
                 }} className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                 </button>
             {/*</Link>*/}
         </form>
