85d493d709f3a453ad5fb176fb3c93ee336c9590
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-15 22:08:12 -0500

    feat: setup deleting films from glitch, tested good

--- src/components/context/glitch/GlitchActions.js
+++ src/components/context/glitch/GlitchActions.js
@@ -5,11 +5,11 @@ const GLITCH_URL = process.env.REACT_APP_GLITCH_URL
 export const getGlitchFilms = async () => {
     const response = await fetch(GLITCH_URL);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     return data.reverse();
 }
 
-export const favoriteGlitchFilm = async (id, updItem) => {
+export const updateGlitchFilm = async (id, updItem) => {
     const response = await fetch(`${GLITCH_URL}/${id}`, {
         method: "PUT",
         body: JSON.stringify(updItem),
@@ -20,5 +20,16 @@ export const favoriteGlitchFilm = async (id, updItem) => {
     const data = await response.json();
     console.log(data);
     return data;
+}
+
+export const deleteGlitchFilm = async (id) => {
+    await fetch(`${GLITCH_URL}/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+
+
 }
 
--- src/components/layout/assets/ModalButtons.jsx
+++ src/components/layout/assets/ModalButtons.jsx
@@ -1,51 +1,75 @@
 import {useContext} from "react";
 import glitchContext from "../../context/glitch/GlitchContext";
-import {favoriteGlitchFilm} from "../../context/glitch/GlitchActions";
+import {deleteGlitchFilm, updateGlitchFilm} from "../../context/glitch/GlitchActions";
 
 const ModalButtons = ( { modalIdObj } ) => {
 
     const { dispatch, glitchFilms } = useContext(glitchContext);
 
-    const handleFavoriteAndWatchlist = async (favorite, watchlist) => {
-        if (typeof favorite === "boolean" && typeof watchlist === "boolean") {
-            dispatch({
-                type: "SET_UPDATING"
-            })
-            const new_obj = {...modalIdObj[0], Favorite: favorite, WatchList: watchlist}
-            const data = await favoriteGlitchFilm(modalIdObj[0].id, new_obj);
-            const new_state = glitchFilms.map(function (film) {
-                if (film.id === modalIdObj[0].id) {
-                    return {...film, ...data}
-                } else {
-                    return film
-                }
-            });
-            dispatch({
-                type: "GET_GLITCH_FILMS",
-                payload: new_state
-            });
+    // const handleFavoriteAndWatchlist = async (favorite, watchlist) => {
+    //     if (typeof favorite === "boolean" && typeof watchlist === "boolean") {
+    //         dispatch({
+    //             type: "SET_UPDATING"
+    //         })
+    //         const new_obj = {...modalIdObj[0], Favorite: favorite, WatchList: watchlist}
+    //         const data = await updateGlitchFilm(modalIdObj[0].id, new_obj);
+    //         const new_state = glitchFilms.map(function (film) {
+    //             if (film.id === modalIdObj[0].id) {
+    //                 return {...film, ...data}
+    //             } else {
+    //                 return film
+    //             }
+    //         });
+    //         dispatch({
+    //             type: "GET_GLITCH_FILMS",
+    //             payload: new_state
+    //         });
+    //     }
+    // }
+
+    const handleDelete = async (id) => {
+        // dispatch({
+        //     type: "SET_LOADING"
+        // });
+        await deleteGlitchFilm(id);
+        let new_state = [];
+        for (let i = 0; i < glitchFilms.length; i++) {
+            if (modalIdObj[0].id !== glitchFilms[i].id ) {
+                new_state = [...new_state, glitchFilms[i]]
+            }
         }
+        dispatch({
+            type: "GET_GLITCH_FILMS",
+            payload: new_state
+        });
+        dispatch({
+            type: "OPEN_GLITCH_FILM",
+            payload: null
+        })
     }
 
     if (modalIdObj.length !== 0) {
-        const { Favorite, WatchList } = modalIdObj[0];
+        const { Favorite, WatchList, id } = modalIdObj[0];
 
         if (!Favorite && WatchList) {
             return (
                 <button onClick={()=> {
-                    handleFavoriteAndWatchlist(true, false);
+                    handleDelete(id);
+                    // handleFavoriteAndWatchlist(true, false);
                 }
-                } className="btn btn-secondary">
-                    Favorite
+                } className="btn btn-primary">
+                    Remove From Watchlist
                 </button>
             )
         } else if (Favorite && !WatchList) {
             return (
                 <button onClick={()=> {
-                    handleFavoriteAndWatchlist(false, true);
+                    // handleFavoriteAndWatchlist(false, true);
+                    handleDelete(id);
                 }
                 } className="btn btn-primary ml-4">
-                    Add To WatchList
+                    UnFavorite
+                    {/*Add To WatchList*/}
                 </button>
             )
         }
