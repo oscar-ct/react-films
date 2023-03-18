2e6bcb3174dc1750751e88ec7210c6d0c5059e0d
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-18 13:49:49 -0500

    update spinner text

--- src/components/layout/LargeSpinner.jsx
+++ src/components/layout/LargeSpinner.jsx
@@ -2,9 +2,8 @@ import spinner from "./assets/loadingGif.gif";
 
 const LargeSpinner = () => {
     return (
-
         <div className="flex flex-col items-center mt-10">
-            <div className="text-white text-4xl mt-10 pb-10">On it, please wait. . .</div>
+            <div className="text-white text-4xl mt-10 pb-10">Loading. . .</div>
             <img
                 src={spinner}
                 alt="Loading..."
